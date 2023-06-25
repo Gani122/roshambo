61265454bf3b629a634ffe41da7b695ae1a24fef
Abdi Ahmed <128744175+Gani122@users.noreply.github.com>
2023-06-25 15:32:22 -0400

    Create function to determine winner of each round

--- rps.js
+++ rps.js
@@ -2,26 +2,50 @@ function getComputerChoice() {
     let choice = "";
     let randomNum = Math.floor(Math.random() * 3);
     if(randomNum === 0) {
-        choice = 'rock'
+        choice = 'ROCK'
     }
     else if (randomNum === 1) {
-        choice = 'paper'
+        choice = 'PAPER'
     }
     else {
-        choice = 'scissors'
+        choice = 'SCISSORS'
     }
 
     return choice;
 }
 
 function shoot(playerSelection, computerSelection) {
+    let round = "";
 
+    if (playerSelection === computerSelection) {
+        round = "Tie!"
+    }
+    else if (playerSelection === 'ROCK' && computerSelection === "PAPER") {
+        round = "You Lose! Paper beats Rock"
+    }
+    else if (playerSelection === 'ROCK' && computerSelection === "SCISSORS") {
+        round = "You Win! Rock beats Scissors"
+    }
+    else if (playerSelection === 'PAPER' && computerSelection === "SCISSORS") {
+        round = "You Lose! Scissors beats Paper"
+    }
+    else if (playerSelection === 'PAPER' && computerSelection === "ROCK") {
+        round = "You Win! Paper beats Rock"
+    }
+    else if (playerSelection === 'SCISSORS' && computerSelection === "ROCK") {
+        round = "You Lose! Rock beats Scissors"
+    }
+    else if (playerSelection === 'SCISSORS' && computerSelection === "PAPER") {
+        round = "You Win! Scissors beats Paper"
+    }
+    
+    return round;
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(computerSelection, playerSelection);
-//console.log (shoot(playerSelection, computerSelection));
+console.log (shoot(playerSelection.toUpperCase(), computerSelection));
+console.log(playerSelection, computerSelection);
 
 function game() {
 
